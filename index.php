fa5c3d68f5a883dafc19daaaa63f149e431ed8b8
natdm02 <natalidimarino2002@gmail.com>
2024-10-10 15:15:56 +0200

    [add] first commit-scaffolding

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once __DIR__ . '/ASSETS/DATA/db.php';
+require_once __DIR__ . '/ASSETS/MODEL/CatShop.php';
+require_once __DIR__ . '/ASSETS/MODEL/DogShop.php';
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>php-oop-2</title>
+
+    <!--bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <!--css-->
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+</head>
+
+<body>
+    <div class="container my-5 text-center">
+
+        <h1><strong>pet shop</strong></h1>
+
+    </div>
+
+    <div class="container my-5">
+
+        <h2> gatti </h2>
+
+    </div>
+
+    <div class="container my-5">
+
+        <h2> cani </h2>
+
+    </div>
+</body>
+</html>
